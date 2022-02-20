3e05e2e40967818dbf22bbd4b704baa1e808cada
Dragosh <stefanescu.dragos@gmail.com>
2022-02-20 12:26:28 +0200

    readme updated

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-
+//Resources used
 //https://www.freecodecamp.org/news/how-to-go-passwordless-with-dotnet-identity/
-//based on this one: https://www.scottbrady91.com/aspnet-identity/implementing-mediums-passwordless-authentication-using-aspnet-core-identity
+//https://www.scottbrady91.com/aspnet-identity/implementing-mediums-passwordless-authentication-using-aspnet-core-identity
 
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -9,16 +9,12 @@ using System.Web;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,21 +24,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = builder.Environment.ApplicationName, Version = "v1" });
-    
+
     #region Bearer
-    c .AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {  
-        Description="JWT authorization using bearer scheme",
-        Name = "Authorization",  
-        Type = SecuritySchemeType.ApiKey,  
-        In = ParameterLocation.Header,  
-    });  
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement  {{  
-        new OpenApiSecurityScheme {  
-            Reference = new OpenApiReference {  
-                Type = ReferenceType.SecurityScheme,  
-                Id = "Bearer"  
-            }  
-        },Array.Empty<string>()}});
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT authorization using bearer scheme",
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        In = ParameterLocation.Header,
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
     #endregion
 });
 
@@ -50,8 +56,6 @@ builder.Services.AddSwaggerGen(c =>
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(o =>
     {
-        //o.DefaultScheme = IdentityConstants.ExternalScheme;
-
         o.TokenValidationParameters = new TokenValidationParameters
         {
             ValidateIssuer = true,
@@ -66,11 +70,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddAuthorization();
 
-// builder.Services.AddAuthentication(options =>
-// {   
-//     options.DefaultScheme = IdentityConstants.ExternalScheme;
-// });
-
 //identity
 builder.Services.AddIdentityCore<IdentityUser>(o =>
     {
@@ -78,19 +77,15 @@ builder.Services.AddIdentityCore<IdentityUser>(o =>
         o.SignIn.RequireConfirmedAccount = false;
         o.SignIn.RequireConfirmedPhoneNumber = false;
     })
-                .AddEntityFrameworkStores<AppDb>()
-                .AddDefaultTokenProviders();
-                 //.AddTokenProvider<TotpSecurityStampBasedTokenProvider<IdentityUser>>(TokenOptions.DefaultEmailProvider);
-               //;
-
+    .AddEntityFrameworkStores<AppDb>()
+    .AddDefaultTokenProviders();
 
 builder.Services.AddDbContext<AppDb>(options =>
-    options.UseInMemoryDatabase(Guid.NewGuid().ToString()),
+        options.UseInMemoryDatabase(Guid.NewGuid().ToString()),
     ServiceLifetime.Singleton);
 
-
 builder.Services.AddScoped<SendGridService>()
-                .AddScoped<TokenService>();
+    .AddScoped<TokenService>();
 
 var app = builder.Build();
 
@@ -103,75 +98,75 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 
-app.MapGet("/", () => "Hello World UnAuthenticated!");
+app.MapGet("/", () => "UnAuthenticated Hello World !");
 
-app.MapGet("/api/{email}",async (string email, UserManager<IdentityUser> _userManager, HttpRequest req, SendGridService sendGridService) =>{
-      // Create or Fetch your user from the database
-    var User = await _userManager.FindByNameAsync(email);
-    if (User == null)
+app.MapGet("/api/{email}",
+    async (string email, UserManager<IdentityUser> _userManager, HttpRequest req, SendGridService sendGridService) =>
     {
-        User = new IdentityUser();
-        User.Email = email;
-        User.UserName = email;
-        var IdentityResult = await _userManager.CreateAsync(User);
-        if (IdentityResult.Succeeded == false)
+        // Create or Fetch your user from the database
+        var User = await _userManager.FindByNameAsync(email);
+        if (User == null)
         {
-            return Results.BadRequest();
+            User = new IdentityUser();
+            User.Email = email;
+            User.UserName = email;
+            var IdentityResult = await _userManager.CreateAsync(User);
+            if (IdentityResult.Succeeded == false)
+            {
+                return Results.BadRequest();
+            }
         }
-    }
 
-    var token = await _userManager.GenerateUserTokenAsync(User,
-        TokenOptions.DefaultProvider, "passwordless");
+        var token = await _userManager.GenerateUserTokenAsync(User,
+            TokenOptions.DefaultProvider, "passwordless");
 
-    var pathurl = req.Host;
-    token = HttpUtility.UrlEncode(token);
-    var url  = $"{req.Scheme}://{req.Host}/api/verify?email={email}&token={token}";
-    Console.WriteLine(url );
+        var pathurl = req.Host;
+        token = HttpUtility.UrlEncode(token);
+        var url = $"{req.Scheme}://{req.Host}/api/verify?email={email}&token={token}";
+        Console.WriteLine(url);
 
-    // DON'T RETURN THE TOKEN.
-    // SEND IT TO THE USER VIA EMAIL.
+        await sendGridService.SendEmail(email, url);
 
-    await sendGridService.SendEmail(email, url);
-
-    return Results.Text("An email was sent to the address provided. Check for the authentication link inside it.");
-});
+        return Results.Text("An email was sent to the address provided. Check for the authentication link inside it.");
+    });
 
 
-app.MapGet("/api/verify",async ( string email, string token, UserManager<IdentityUser> _userManager, TokenService tokenService) =>{
-    Console.WriteLine($"Received email: {email} and token:{token}");
-
-    // Fetch your user from the database
-    var User = await _userManager.FindByNameAsync(email);
-   
-    if (User == null)
-     return Results.NotFound();
-    
-    Console.WriteLine($"Verify user found: {User.Email}");
-
-    var IsValid = await _userManager.
-        VerifyUserTokenAsync(User, TokenOptions.DefaultProvider, "passwordless",token.Trim());
-    if (IsValid)
+app.MapGet("/api/verify",
+    async (string email, string token, UserManager<IdentityUser> _userManager, TokenService tokenService) =>
     {
-        // TODO: Generate a bearer token
-        var BearerToken = tokenService.GetToken(User);
-        Console.WriteLine("All good returning  bearer:" + BearerToken);
-        return Results.Text( BearerToken);
-    }
-    return Results.Unauthorized();
-    
-});
+        Console.WriteLine($"Received email: {email} and token:{token}");
 
+        // Fetch your user from the database
+        var user = await _userManager.FindByNameAsync(email);
 
-app.MapGet("/api/dashboard", (ClaimsPrincipal user)=> Results.Ok($"{user.Identity?.Name} is Authenticated! " )).RequireAuthorization();
+        if (user == null)
+            return Results.NotFound();
+
+        Console.WriteLine($"Verify user found: {user.Email}");
+
+        var isValid =
+            await _userManager.VerifyUserTokenAsync(user, TokenOptions.DefaultProvider, "passwordless", token.Trim());
+        if (isValid)
+        {
+            // TODO: Generate a bearer token
+            var bearerToken = tokenService.GetToken(user);
+            Console.WriteLine("All good returning  bearer:" + bearerToken);
+            return Results.Text(bearerToken);
+        }
+
+        return Results.Unauthorized();
+    });
+
+app.MapGet("/api/dashboard", (ClaimsPrincipal user) => Results.Ok($"{user.Identity?.Name} is Authenticated! "))
+    .RequireAuthorization();
 
 app.Run();
 
 
-
-public class AppDb: IdentityDbContext{
-    public AppDb(DbContextOptions<AppDb> options): base(options)
+public class AppDb : IdentityDbContext
+{
+    public AppDb(DbContextOptions<AppDb> options) : base(options)
     {
-        
     }
 }
 
@@ -185,18 +180,18 @@ public class SendGridService
     public async Task SendEmail(string email, string url)
     {
         var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY") ?? _configuration["SENDGRID_API_KEY"];
+        var fromEmail = Environment.GetEnvironmentVariable("FROM_EMAIL") ?? _configuration["FROM_EMAIL"];
         var client = new SendGridClient(apiKey);
-        var from = new EmailAddress("test@passwordless.com", "Passwordless login");
+        var from = new EmailAddress(fromEmail, "Passwordless login");
         var subject = "Passwordless login link";
         var to = new EmailAddress(email);
-        var htmlContent = $"<h2>Click the link below to login to passwwordless</h2><br/><h3> <a href="+url+"> Login</a></h3>";
+        var htmlContent = $"<h2>Click the link below to login to passwwordless</h2><br/><h3> <a href=" + url +
+                          "> Login</a></h3>";
         var msg = MailHelper.CreateSingleEmail(from, to, subject, "", htmlContent);
         var response = await client.SendEmailAsync(msg);
         Console.WriteLine("Sendgrid response:" + JsonConvert.SerializeObject(response));
-
     }
 }
-
 
 
 public class TokenService
@@ -211,10 +206,10 @@ public class TokenService
         var validAudience = _configuration["Jwt:Audience"];
         var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
         var credentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
-        var tokenClaims = new JwtSecurityToken(claims: new []
+        var tokenClaims = new JwtSecurityToken(claims: new[]
             {
-                new Claim("name",user.Email), 
-                new Claim(  "sub",  user.Id.ToString())
+                new Claim("name", user.Email),
+                new Claim("sub", user.Id.ToString())
             }, issuer: validIssuer, audience: validAudience,
             signingCredentials: credentials);
         var token = new JwtSecurityTokenHandler().WriteToken(tokenClaims);
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Minimal API  passwordless
+# Minimal API PasswordLess
 
 Passwordless authentication and authorization with .Net 6 Minimal Api + Microsoft IdentityModel JWT + In Memory EF database + SendGrid
 
@@ -8,4 +8,22 @@ The code in the repo contains all the endpoints needed to create passwordless au
 
 The missing SendGrid key must be created at SendGrid side and added to the settings file in order to have the code working E2E
 
-# Content TBD
+## How it is supposed to be used
+
+1. Inside `appsettings.json` update the keys: `Issuer`, `Audience`,`Key`, `FROM_EMAIL` and `SENDGRID_API_KEY` with your own. SendGrid api key can be created on their site and used for free with some wide limits
+2. Deploy the repo code to Azure or Heroku or any other hosting server
+3. Open the `[YOUR_URL]/swagger` url
+4. Use the endpoint `/api/{email}` with your email or a test email.
+5. Check the inbox for the email specified above. The email sent from passwordless via sendgrid might arrive in your `SPAM` folder.
+6. Click on the `Login` link in the email and copy the JWT token displayed in the page 
+7. Use the api Swagger's `Authorize` button and add the token as: `Bearer [YOUR_TOKEN_HERE]`
+8. Test the `/api/dashboard` from swagger and if the bearer is valid you should see the message returned `"[YOUR_EMAIL_HERE] is Authenticated! "`
+
+**The api can be used for any SPA that needs passwordless authentication with a backend written with .NET 6 Minimal API.**
+
+<br>
+<br>
+Happy coding! <br>
+@Imhotepp
+
+
